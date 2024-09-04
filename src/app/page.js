f2c3ffc717e1bb888c5cf61b16bfabd8bbcfd117
mugihouse <mugi.stnk100122@gmail.com>
2024-09-04 21:05:00 +0900

    Add: Navbarコンポーネントをインポート

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import ClientComp from "./components/ClientComp";
+import Navbar from "./components/Navbar/Navbar";
 
 export const ENDPOINT_URL = "https://pokeapi.co/api/v2/pokemon";
 
 export default function Home() {
   return (
-    <div>
+    <>
+      <Navbar />
       <ClientComp />
-    </div>
+    </>
   );
 }
