eff7f6bd95c544223eb2e6208b33d70ab7664d09
mugihouse <mugi.stnk100122@gmail.com>
2024-09-04 21:07:22 +0900

    Add: 「次へ」「前へ」ボタンを追加

--- src/app/components/ClientComp.js
+++ src/app/components/ClientComp.js
@@ -9,12 +9,16 @@ import Card from "./Card/Card";
 const ClientComp = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
+  const [prevURL, setPrevURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       const res = await getAllPokemon(ENDPOINT_URL);
       // 各ポケモンのデータを取得
       loadPokemon(res.results);
+      setNextURL(res.next);
+      setPrevURL(res.previous);
       setIsLoading(false);
     };
     fetchPokemonData();
@@ -30,7 +34,26 @@ const ClientComp = () => {
     setPokemonData(_pokemonData);
   };
 
-  console.log(pokemonData);
+  // console.log(pokemonData);
+
+  const handlePrevPage = async () => {
+    if (!prevURL) return;
+
+    setIsLoading(true);
+    const data = await getAllPokemon(prevURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setIsLoading(false);
+  };
+  const handleNextPage = async () => {
+    setIsLoading(true);
+    const data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setNextURL(data.next);
+    setPrevURL(data.previous);
+    setIsLoading(false);
+  };
   return (
     <>
       {isLoading ? (
@@ -44,6 +67,11 @@ const ClientComp = () => {
           </div>
         </>
       )}
+      <div className="btn">
+        {prevURL ? <button onClick={handlePrevPage}>前へ</button> : <div></div>}
+
+        <button onClick={handleNextPage}>次へ</button>
+      </div>
     </>
   );
 };
